238914237d6f80aceab35e38c191dc0a73a53a89
krr0ption <mardin-pop@protonmail.com>
2020-09-09 22:32:58 +0300

    fix: set argument for base class.

--- src/DataStructure/Collection.hpp
+++ src/DataStructure/Collection.hpp
@@ -7,21 +7,20 @@ namespace mlib {
 	template<typename T>
 	class Collection {
 	public:
-		virtual T get() = 0;
-		virtual void append_front() = 0;
-		virtual void append_back() = 0;
+		virtual void append_front(T value) = 0;
+		virtual void append_back(T value) = 0;
 		virtual void remove_back() = 0;
 		virtual void remove_front() = 0;
-		virtual void remove() = 0;
-		virtual void insert() = 0;
-		virtual void swap() = 0;
+		virtual void remove(index_t index) = 0;
+		virtual void insert(index_t index,T value) = 0;
 		virtual void reverse() = 0;
+		virtual void swap(index_t index_1,index_t index_2) = 0;
 		virtual void print()  = 0;
-		virtual void strip() = 0;
-		virtual void push_front() = 0;
-		virtual void push_back() = 0;
+		virtual void strip(T value) = 0;
+		virtual void push_front(T value) = 0;
+		virtual void push_back(T value) = 0;
 		virtual void purge() = 0;
-		virtual index_t search() = 0;
+		virtual index_t search(T value) = 0;
 		virtual T begin() = 0;
 		virtual T end() = 0;
 		virtual T pop_front() = 0;
