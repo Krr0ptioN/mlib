0476f856c1a3c388bb0fb9cf965f285568d8e578
krr0ption <mardin-pop@protonmail.com>
2020-09-09 15:16:29 +0300

    fix: Array class methods

--- src/DataStructure/Array.hpp
+++ src/DataStructure/Array.hpp
@@ -11,35 +11,39 @@ namespace mlib {
 	private:
 		T* list;
 		index_t size = 0;
+		index_t length = 0;
+		void resize(index_t add_size);
 	public:
 		
 		array(index_t entr_size);
 		array(const array<T> &array2);
-
 		~array();
-		
-		index_t length = 0;
-		inline index_t getSize();
-		inline T get(index_t index);
-		void resize(index_t add_size);
-		void append(T value);
-		void append(array& array2);
-		index_t search(T value);
-		void remove(index_t index);
+		void append_back(T value);
+		void append_front(T value);
+		void append_back(array& array2);
+		void remove_back(index_t index);
+		void remove_front(index_t index);
 		void insert(index_t index,T value);
-		inline void swap(index_t index,index_t index2);
+		void swap(index_t index,index_t index2);
 		void reverse();
 		void print();
 		void shiftRight();
 		void shiftLeft();
 		void strip(T value);
 		void freedup();
+		void purge();
+		void push_front(T value);
+		void push_back(T value);
+		index_t getLength();
+		index_t getSize();
+		index_t search(T value);
+		T get(index_t index);
+	 	T pop_front();
+	 	T pop_back();
 		inline T begin();
 		inline T end();
-		inline T pop_front();
-		inline T pop_back();
-		inline void push_front(T value);
-		inline void push_back(T value);
+		
+		
 		
 		T operator[](index_t index);
 	};
